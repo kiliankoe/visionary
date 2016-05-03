e106a76ab1f19a2b0728f354bafde83e4384e1d8
Kilian Koeltzsch <me@kilian.io>
2016-05-03 16:23:53 +0200

    try sample code from sodium readme

--- Visionary/ViewController.swift
+++ Visionary/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import Sodium
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let sodium = Sodium()!
+        let password = "Correct Horse Battery Staple".dataUsingEncoding(NSUTF8StringEncoding)!
+        let hashedStr = sodium.pwHash.scrypt.str(password, opsLimit: sodium.pwHash.scrypt.OpsLimitInteractive, memLimit: sodium.pwHash.scrypt.MemLimitInteractive)!
+
+        if sodium.pwHash.scrypt.strVerify(hashedStr, passwd: password) {
+            print("PW matches")
+        } else {
+            print("PW doesn't match")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
